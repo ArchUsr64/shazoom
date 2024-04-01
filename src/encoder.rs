ac36ac11f6aac7c3496b84e8a8e96690fb9a306b
Anshul Dalal <anshulusr@gmail.com>
2024-04-01 18:29:48 +0530

    encoder: add bucket wise frequency computation

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,158 +1,77 @@
 //! Handles conversion of a WAV file on disk into a vector of Frequency signatures
-use std::fmt::Debug;
 
-use rayon::prelude::*;
-use serde::{Deserialize, Serialize};
+use easyfft::prelude::DynRealFft;
 
-/// Store signature for every sample window with this time interval
-const SAMPLE_WINDOW_LENGTH_MS: usize = 100;
-
-/// Minimum frequency to test
-const MIN_FREQ: usize = 100;
-/// Maximum frequency to test
-const MAX_FREQ: usize = 900;
-
-/// The entire `MIN_FREQ..MAX_FREQ` range is split into `FREQ_BUCKETS`,
-/// within each bucket range, the most prominent frequency is stored
-const FREQ_BUCKETS_COUNT: usize = 8;
-
-/// AND the frequencies with this factor to account for background noise during
-/// lookup
-const FUZZ_FACTOR: usize = 0b0;
-
-/// Represents the sound fingerprint of a slice
-/// The n most prominent frequenies of each `FREQ_BUCKET` is stored as a single
-/// byte in the u64 in native endianness
-#[derive(Serialize, Deserialize, Clone, Copy)]
-pub struct Signature(u64);
-
-impl Debug for Signature {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		let mut buffer = "{".to_string();
-		let bucket_size = (MAX_FREQ - MIN_FREQ) / FREQ_BUCKETS_COUNT;
-		self.0
-			.to_ne_bytes()
-			.iter()
-			.enumerate()
-			.for_each(|(i, &byte)| {
-				buffer.push_str(&format!(
-					"[{}-{}]:{}, ",
-					i * bucket_size + MIN_FREQ,
-					(i + 1) * bucket_size + MIN_FREQ,
-					byte as usize + i * bucket_size + MIN_FREQ,
-				))
-			});
-		buffer.pop().unwrap();
-		buffer.pop().unwrap();
-		write!(f, "{}}}", buffer)
-	}
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub struct Song {
-	pub signatures: Vec<Signature>,
+	pub sample_rate: usize,
+	pub samples: Vec<f32>,
 }
-
 impl Song {
-	/// Expects a .wav file name without the extension in the `songs` director
-	pub fn from_file(file_path: String) -> Option<Song> {
-		let (sample_rate, buffer) = parse_wav(file_path)?;
-		let sample_window_width = sample_rate as usize * SAMPLE_WINDOW_LENGTH_MS / 1000;
-		let sample_window_count = buffer.len() / sample_window_width;
-		let signatures = (0..sample_window_count)
-			.into_par_iter()
-			.map(|window_index| {
-				(
-					window_index * sample_window_width,
-					(window_index + 1) * sample_window_width,
-				)
-			})
-			.map(|(sample_init, sample_end)| {
-				let sample_window = &buffer[sample_init..sample_end];
-				let bucket_size = (MAX_FREQ - MIN_FREQ) / FREQ_BUCKETS_COUNT;
-				(0..FREQ_BUCKETS_COUNT)
-					.map(move |bucket_index| {
-						let bucket_range = (bucket_index * bucket_size + MIN_FREQ)
-							..((bucket_index + 1) * bucket_size + MIN_FREQ);
-						bucket_range
-							.map(|test_freq| {
-								fourier_transform(sample_window, sample_rate, test_freq as f32)
-							})
-							.enumerate()
-							.max_by(|(_, x), (_, y)| x.partial_cmp(y).unwrap())
-							.unwrap()
-					})
-					.enumerate()
-					.fold(0, |acc, (bucket_index, (freq, _freq_amplitude))| {
-						acc | (((freq % bucket_size) & !FUZZ_FACTOR) << (bucket_index * 8))
-					})
-			})
-			.map(|signature_data| Signature(signature_data as u64))
-			.collect();
-		Some(Song { signatures })
-	}
-}
-
-pub fn parse_wav(file_path: String) -> (f32, Vec<f32>) {
-	let byte_array = std::fs::read(file_path).unwrap();
-	let channel_count = u16::from_le_bytes([byte_array[22], byte_array[23]]);
-	assert_eq!(channel_count, 1);
-	let sample_rate = u16::from_le_bytes([byte_array[24], byte_array[25]]);
-	assert_eq!(sample_rate, 44100);
-	let mut data = Vec::new();
-	let mut byte_iter = byte_array.iter();
-	while let Some(&lsb) = byte_iter.next() {
-		if let Some(&msb) = byte_iter.next() {
-			let word_data = i16::from_le_bytes([lsb, msb]);
-			data.push(word_data as f32 / i16::MAX as f32);
+	pub fn from_wav(file_path: String) -> Song {
+		let byte_array = std::fs::read(file_path).unwrap();
+		let channel_count = u16::from_le_bytes([byte_array[22], byte_array[23]]);
+		assert_eq!(channel_count, 1, "Only mono channel files are supported!");
+		let sample_rate = u16::from_le_bytes([byte_array[24], byte_array[25]]) as usize;
+		let mut samples = Vec::new();
+		let mut byte_iter = byte_array.iter();
+		while let Some(&lsb) = byte_iter.next() {
+			if let Some(&msb) = byte_iter.next() {
+				let word_data = i16::from_le_bytes([lsb, msb]);
+				samples.push(word_data as f32 / i16::MAX as f32);
+			}
+		}
+		Song {
+			sample_rate,
+			samples,
 		}
 	}
-	(
-		sample_rate as f32,
-		// First 44 bytes are metatdata as per the WAV spec
-		data,
-	)
-}
 
-pub fn fourier_transform(data: &[f32], sample_rate: f32, test_frequency: f32) -> f32 {
-	let scalar = 2. * PI * test_frequency / sample_rate as f32;
-	use std::f32::consts::PI;
-	let real_part: f32 = data
-		.iter()
-		.enumerate()
-		.map(|(i, sample)| sample * (i as f32 * scalar).cos())
-		.sum();
-	let img_part: f32 = data
-		.iter()
-		.enumerate()
-		.map(|(i, sample)| sample * (i as f32 * scalar).sin())
-		.sum();
-	(real_part.powi(2) + img_part.powi(2)).sqrt() / data.len() as f32
-}
-
-/// Expects a 100ms slice of audio sampled at 44100Hz and returns the 8 most prominent
-/// frequencies in each 100Hz window from 100 to 900Hz
-///
-/// The output frequencies are multiples of 10Hz since the sample is 1/10 of a second
-pub fn fast_fourier_transform(data: &[f32]) -> [usize; 8] {
-	assert_eq!(data.len(), 4410);
-	use easyfft::prelude::*;
-	let frequencies: Vec<_> = data
-		.real_fft()
-		.iter()
-		.map(|i| i.norm())
-		.skip(10)
-		.take(80)
-		.collect();
-	let mut res = [0; 8];
-	for (i, max_freq) in res.iter_mut().enumerate() {
-		*max_freq = (i + 1) * 100
-			+ 10 * frequencies[i * 10..(i + 1) * 10]
-				.iter()
-				.enumerate()
-				.max_by(|(_, &x), (_, y)| x.partial_cmp(y).unwrap())
-				.map(|(i, _)| i)
-				.unwrap();
+	/// For each time slice of duration `slice_size`, compute the frequency with the
+	/// highest amplitude for each frequency bucket.
+	///
+	/// The frequency range spans from 0 to `bucket_size` * `bucket_count`
+	pub fn constelation_map(
+		&self,
+		slice_size: std::time::Duration,
+		bucket_size: usize,
+		bucket_count: usize,
+	) -> Vec<Vec<usize>> {
+		let sample_window_size = self.sample_rate * slice_size.as_millis() as usize / 1000;
+		let mut fft_extended_buffer = vec![0f32; self.sample_rate];
+		self.samples
+			.chunks_exact(sample_window_size)
+			.map(|slice| {
+				slice
+					.iter()
+					.zip(fft_extended_buffer.iter_mut())
+					.for_each(|(&sample, buffer)| *buffer = sample);
+				let freq_amplitudes: Vec<_> = fft_extended_buffer
+					.real_fft()
+					.iter()
+					.map(|i| i.norm())
+					.take(bucket_size * bucket_count)
+					.enumerate()
+					.collect();
+				let mut bucket_frequencies: Vec<_> = freq_amplitudes
+					.chunks_exact(bucket_size)
+					.map(|freq_bucket| {
+						freq_bucket
+							.iter()
+							.max_by(|(_freq_1, ampl_1), (_freq_2, ampl_2)| {
+								ampl_1.partial_cmp(ampl_2).unwrap()
+							})
+							.unwrap()
+					})
+					.collect();
+				bucket_frequencies.sort_unstable_by(|(_freq_1, ampl_1), (_freq_2, ampl_2)| {
+					ampl_2.partial_cmp(ampl_1).unwrap()
+				});
+				bucket_frequencies
+					.iter()
+					.map(|(freq, _ampl)| *freq)
+					.collect()
+			})
+			.collect()
 	}
-	res
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-#![feature(array_chunks)]
-
-mod database;
 mod encoder;
+use easyfft::prelude::*;
 
 fn main() {
-	env_logger::init();
-	let _ = database::DatabaseBuilder::new().add_song("500_hz").build();
+	let song = encoder::Song::from_wav("songs/tvari-hawaii-vacation-159069 (1).mp3.wav".into());
+	let constillation_map = song.constelation_map(std::time::Duration::from_millis(100), 100, 16);
+	for (i, slice) in constillation_map.iter().enumerate() {
+		for freq in &slice[..4] {
+			println!("{i}, {freq}")
+		}
+	}
 }
