92424db49414bc6f84ee50f70d2a695ddc5f3634
Anshul Dalal <anshulusr@gmail.com>
2024-04-11 10:24:59 +0530

    db,encoder: defer data collection by passing Iters

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,6 @@
 //! Handles management of the song fingerprints
 
+use rayon::prelude::*;
 use rustc_hash::FxHashMap;
 
 use crate::encoder::{self, Freq, Signature, TimeStamp};
@@ -26,7 +27,10 @@ impl Default for DatabaseConfig {
 	}
 }
 impl DatabaseConfig {
-	pub fn signatures(&self, song: encoder::Song) -> Vec<Vec<Signature>> {
+	pub fn signatures<'a>(
+		&'a self,
+		song: &'a encoder::Song,
+	) -> impl Iterator<Item = Vec<Signature>> + 'a {
 		let constellation_map = song.constellation_map(
 			self.slice_size,
 			self.freq_per_slice,
@@ -38,15 +42,12 @@ impl DatabaseConfig {
 			self.target_zone_size.1,
 			constellation_map,
 		);
-		signatures
-			.iter()
-			.map(|signatures| {
-				signatures
-					.iter()
-					.map(|&signature| signature.fuzz(self.fuzz_factor))
-					.collect()
-			})
-			.collect()
+		signatures.map(|signatures| {
+			signatures
+				.iter()
+				.map(|&signature| signature.fuzz(self.fuzz_factor))
+				.collect()
+		})
 	}
 }
 
@@ -60,23 +61,29 @@ impl DatabaseBuilder {
 	}
 	pub fn build(&self, config: DatabaseConfig) -> Database {
 		let mut db = Database::new(config);
-		self.songs_path
-			.iter()
-			.enumerate()
-			.for_each(|(song_id, song_path)| {
-				println!("{song_id}");
-				let song = encoder::Song::from_wav(song_path);
-				config
-					.signatures(song)
+		let song_signatures = |song_id| -> Vec<(Signature, (usize, TimeStamp))> {
+			let song = encoder::Song::from_wav(&self.songs_path[song_id]);
+			let signatures = config.signatures(&song);
+			// TODO: set an estimated initial capacity
+			let mut res = Vec::new();
+			signatures.enumerate().for_each(|(timestamp, signature)| {
+				signature
 					.iter()
-					.enumerate()
-					.for_each(|(timestamp, signature)| {
-						signature.iter().for_each(|&signature| {
-							let vec = db.data.entry(signature).or_insert(Vec::new());
-							vec.push((song_id, timestamp as TimeStamp));
-						})
-					})
+					.copied()
+					.for_each(|i| res.push((i, (song_id, timestamp as TimeStamp))))
 			});
+			res
+		};
+		let data: Vec<Vec<(Signature, (usize, TimeStamp))>> = self
+			.songs_path
+			.par_iter()
+			.enumerate()
+			.map(|(i, _path)| song_signatures(i))
+			.collect();
+		for (signature, value) in data.iter().map(|i| i.iter()).flatten() {
+			let vec = db.data.entry(*signature).or_insert(Vec::new());
+			vec.push(*value);
+		}
 		db
 	}
 }
@@ -99,8 +106,10 @@ impl Database {
 	}
 	pub fn match_sample(&self, sample: encoder::Song) -> Vec<(usize, usize)> {
 		let mut song_offsets: FxHashMap<usize, FxHashMap<i32, usize>> = FxHashMap::default();
-		self.config.signatures(sample).iter().enumerate().for_each(
-			|(sample_timestamp, signatures)| {
+		self.config
+			.signatures(&sample)
+			.enumerate()
+			.for_each(|(sample_timestamp, signatures)| {
 				signatures.iter().for_each(|i| {
 					if let Some(matches) = self.data.get(i) {
 						matches.iter().for_each(|(song_id, song_timestamp)| {
@@ -112,8 +121,7 @@ impl Database {
 						})
 					}
 				})
-			},
-		);
+			});
 		song_offsets
 			.iter()
 			.map(|(&song_id, offset_freq_table)| {
@@ -122,10 +130,7 @@ impl Database {
 					offset_freq_table
 						.iter()
 						.max_by_key(|i| i.1)
-						.map(|i| {
-							println!("Offset: {i:?}");
-							i.1
-						})
+						.map(|i| i.1)
 						.copied()
 						.unwrap(),
 				)
--- src/encoder.rs
+++ src/encoder.rs
@@ -1,6 +1,8 @@
 //! Handles conversion of a WAV file on disk into a vector of Frequency signatures
 
-use easyfft::prelude::DynRealFft;
+use std::hash::Hash;
+
+use easyfft::prelude::*;
 
 pub type Freq = u16;
 pub type TimeStamp = u32;
@@ -17,6 +19,7 @@ impl Signature {
 #[derive(Debug, Clone)]
 pub struct Song {
 	pub sample_rate: usize,
+	// TODO: will i16 samples work here??
 	pub samples: Vec<f32>,
 }
 impl Song {
@@ -25,12 +28,12 @@ impl Song {
 		let channel_count = u16::from_le_bytes([byte_array[22], byte_array[23]]);
 		assert_eq!(channel_count, 1, "Only mono channel files are supported!");
 		let sample_rate = u16::from_le_bytes([byte_array[24], byte_array[25]]) as usize;
-		let mut samples = Vec::new();
+		let mut samples = Vec::with_capacity(byte_array.len() / 2);
 		let mut byte_iter = byte_array.iter();
 		while let Some(&lsb) = byte_iter.next() {
 			if let Some(&msb) = byte_iter.next() {
 				let word_data = i16::from_le_bytes([lsb, msb]);
-				samples.push(word_data as f32 / i16::MAX as f32);
+				samples.push(word_data as f32);
 			}
 		}
 		Song {
@@ -43,47 +46,43 @@ impl Song {
 		std::time::Duration::from_millis((self.samples.len() * 1000 / self.sample_rate) as u64)
 	}
 
-	pub fn signatures(
+	pub fn signatures<'a, T: ExactSizeIterator<Item = Vec<Freq>>>(
 		// This should be a `std::time::Duration`
 		target_zone_width: TimeStamp,
 		target_zone_height: Freq,
-		constellation_map: Vec<Vec<Freq>>,
-	) -> Vec<Vec<Signature>> {
-		(0..constellation_map.len() - 1)
-			.map(|i| {
-				let slice = &constellation_map[i];
-				let target_slices = &constellation_map
-					[i..constellation_map.len().min(i + target_zone_width as usize)];
-				slice
-					.iter()
-					.copied()
-					.map(move |anchor_freq| {
-						target_slices
-							.iter()
-							.enumerate()
-							.skip(1)
-							.map(move |(time_offset, target_slice)| {
-								target_slice
-									.iter()
-									.copied()
-									.filter(move |target_freq| {
-										(anchor_freq.saturating_sub(target_zone_height / 2)
-											..anchor_freq + target_zone_height / 2)
-											.contains(target_freq)
-									})
-									.map(move |target_freq| {
-										Signature(
-											(anchor_freq, target_freq),
-											time_offset as TimeStamp,
-										)
-									})
-							})
-							.flatten()
-					})
-					.flatten()
-					.collect()
-			})
-			.collect()
+		constellation_map: T,
+	) -> impl Iterator<Item = Vec<Signature>> + 'a {
+		let constellation_map: Vec<_> = constellation_map.collect();
+		(0..constellation_map.len() - 1).map(move |i| {
+			let slice = &constellation_map[i];
+			let target_slices =
+				&constellation_map[i..constellation_map.len().min(i + target_zone_width as usize)];
+			slice
+				.iter()
+				.copied()
+				.map(move |anchor_freq| {
+					target_slices
+						.iter()
+						.enumerate()
+						.skip(1)
+						.map(move |(time_offset, target_slice)| {
+							target_slice
+								.iter()
+								.copied()
+								.filter(move |target_freq| {
+									(anchor_freq.saturating_sub(target_zone_height / 2)
+										..anchor_freq + target_zone_height / 2)
+										.contains(target_freq)
+								})
+								.map(move |target_freq| {
+									Signature((anchor_freq, target_freq), time_offset as TimeStamp)
+								})
+						})
+						.flatten()
+				})
+				.flatten()
+				.collect()
+		})
 	}
 
 	/// For each time slice of duration `slice_size`, compute the frequency with the
@@ -96,12 +95,12 @@ impl Song {
 		freq_per_slice: usize,
 		bucket_size: Freq,
 		bucket_count: usize,
-	) -> Vec<Vec<Freq>> {
+	) -> impl ExactSizeIterator<Item = Vec<Freq>> + '_ {
 		let sample_window_size = self.sample_rate * slice_size.as_millis() as usize / 1000;
 		let mut fft_extended_buffer = vec![0f32; self.sample_rate];
 		self.samples
 			.chunks_exact(sample_window_size)
-			.map(|slice| {
+			.map(move |slice| {
 				slice
 					.iter()
 					.zip(fft_extended_buffer.iter_mut())
@@ -134,6 +133,5 @@ impl Song {
 					.take(freq_per_slice)
 					.collect()
 			})
-			.collect()
 	}
 }
